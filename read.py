45b91ac0f2d7a52bd4bc65e00025cf5611f0e18c
Tom Chan <tchan.cuhk@gmail.com>
2021-06-09 14:56:41 +0800

    add the screening criteria of the word or letter

--- read.py
+++ read.py
@@ -19,3 +19,23 @@ sum_len = 0
 for d in data:
 	sum_len = sum_len + len(d)
 print('The average number of letter in each review is ', sum_len/len(data))
+
+# to screen the review by the number of words or letters
+
+new1 = []
+new2 = []
+l = 0
+w = 0
+for d in data:
+	if len(d) > 200:
+		new1.append(d)
+		l += 1
+	if len(d.split()) >100:
+		new2.append(d)
+		w += 1
+print('There are ', l, 'comments with more than 200 letters.')
+print('There are ', w, 'comments with more than 100 words')
+print('Total ', len(new1), 'comments with more than 200 letters')
+print(new1[0])
+print('Total ', len(new2), 'comments with more than 100 words')
+print(new2[0])
